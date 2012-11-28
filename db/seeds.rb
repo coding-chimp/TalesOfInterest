f760be8fc70ab715dda205714dc995a56d4d692c
Bastian <xarfai27@gmail.com>
2012-11-28 20:46:50 +0100

    commented seed data out

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,46 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Podcasts
-@recordcase = Podcast.create!(:name => "Recordcase", :description => "A podcast about music.")
-@celluloid = Podcast.create!(:name => "Celluloid", :description => "A podcast about movies.")
-
-# Episodes Recordcase
-Episode.create!(:podcast => @recordcase, :number => 1, :title => "GEMA, GVL und CCmixter", 		:description => "", :created_at => "2010-09-26")
-Episode.create!(:podcast => @recordcase, :number => 2, :title => "Relax and Enjoy", 			:description => "", :created_at => "2010-10-12")
-Episode.create!(:podcast => @recordcase, :number => 3, :title => "be myself", 					:description => "", :created_at => "2010-11-21")
-Episode.create!(:podcast => @recordcase, :number => 4, :title => "Spanish Jazz", 				:description => "", :created_at => "2010-12-05")
-Episode.create!(:podcast => @recordcase, :number => 5, :title => "Survive", 					:description => "", :created_at => "2011-09-24")
-Episode.create!(:podcast => @recordcase, :number => 6, :title => "John Williams", 				:description => "", :created_at => "2011-11-16")
-Episode.create!(:podcast => @recordcase, :number => 7, :title => "John Williams die Zweite", 	:description => "", :created_at => "2012-01-01")
-Episode.create!(:podcast => @recordcase, :number => 8, :title => "Alan Silvestri", 				:description => "", :created_at => "2012-03-12")
-
-# Episodes Celluloid
-Episode.create!(:podcast => @celluloid, :number => 1, 	:title => "Pulp Fiction: Tarantino", 																:description => "", :created_at => "2010-09-26")
-Episode.create!(:podcast => @celluloid, :number => 2, 	:title => "Alfred Hitchcock und Der unsichtbare Dritte", 											:description => "", :created_at => "2010-10-13")
-Episode.create!(:podcast => @celluloid, :number => 3, 	:title => "David Fincher and The Game.", 															:description => "", :created_at => "2010-11-21")
-Episode.create!(:podcast => @celluloid, :number => 4, 	:title => "Stanley Kubrick und seine Space Opera!",													:description => "", :created_at => "2010-12-05")
-Episode.create!(:podcast => @celluloid, :number => 5, 	:title => "Ivan Reitmann und wie man am besten Geister auf der Leinwand faengt", 					:description => "", :created_at => "2011-04-13")
-Episode.create!(:podcast => @celluloid, :number => 6, 	:title => "Alien und sein Schoepfer Ridley Scott", 													:description => "", :created_at => "2011-06-26")
-Episode.create!(:podcast => @celluloid, :number => 7, 	:title => "Ian Flemmings- Bond,,,, James Bond", 													:description => "", :created_at => "2011-06-30")
-Episode.create!(:podcast => @celluloid, :number => 8, 	:title => "Christopher Nolan: Memento und Batman begins", 											:description => "", :created_at => "2011-07-10")
-Episode.create!(:podcast => @celluloid, :number => 9, 	:title => "Darren Aronofsky Black Swan / The Wrestler",												:description => "", :created_at => "2011-09-21")
-Episode.create!(:podcast => @celluloid, :number => 10, 	:title => "Steven Spielberg and the green little thing", 											:description => "", :created_at => "2011-11-16")
-Episode.create!(:podcast => @celluloid, :number => 11, 	:title => "George Lucas Star Wars Saga (oder warum man keine schlechten Prequels drehen sollte)",	:description => "", :created_at => "2011-11-17")
-Episode.create!(:podcast => @celluloid, :number => 12, 	:title => "Robert Zemeckis und Back to the future", 												:description => "", :created_at => "2012-01-03")
-Episode.create!(:podcast => @celluloid, :number => 13, 	:title => "Merry Christmas und TOI lieblings Weihnachtsfilme", 										:description => "", :created_at => "2012-01-03")
-Episode.create!(:podcast => @celluloid, :number => 14, 	:title => "Bud Spencer & Terence Hill",																:description => "", :created_at => "2012-03-12")
-Episode.create!(:podcast => @celluloid, :number => 15, 	:title => "Peter Jackson", 																			:description => "", :created_at => "2012-06-02")
-Episode.create!(:podcast => @celluloid, :number => 16,	:title => "Joss Whedon und The Avengers", 															:description => "", :created_at => "2012-06-22")
-Episode.create!(:podcast => @celluloid, :number => 17,	:title => "Charlie Chaplin", 																		:description => "", :created_at => "2012-07-20")
-Episode.create!(:podcast => @celluloid, :number => 18,	:title => "Guy Ritchie",																			:description => "", :created_at => "2012-10-19")
-
-# Pages
-Page.create!(:titel => "About", :content => "")
-Page.create!(:titel => "Impressum", :content => "")
-
-# Blogroll
-Blogroll.create!(:name => "Code-Chimp", :url => "http://code-chimp.org", :description => "Bastians Tech-Blog")
-Blogroll.create!(:name => "Movie-Trivia of the Day", :url => "http://movietriviaoftheday.com/", :description => "Bastians Trivia-Seite")
-Blogroll.create!(:name => "Kinosaal Deluxe", :url => "http://kinosaal-deluxe.de/", :description => "Dimitars Filmrezensionen")
-Blogroll.create!(:name => "Der Lautsprecher", :url => "http://der-lautsprecher.de/", :description => "Podcast ueber Podcasting von Tim Pritlove")
+## Podcasts
+#@recordcase = Podcast.create!(:name => "Recordcase", :description => "A podcast about music.")
+#@celluloid = Podcast.create!(:name => "Celluloid", :description => "A podcast about movies.")
+#
+## Episodes Recordcase
+#Episode.create!(:podcast => @recordcase, :number => 1, :title => "GEMA, GVL und CCmixter", 		:description => "", :created_at => "2010-09-26")
+#Episode.create!(:podcast => @recordcase, :number => 2, :title => "Relax and Enjoy", 			:description => "", :created_at => "2010-10-12")
+#Episode.create!(:podcast => @recordcase, :number => 3, :title => "be myself", 					:description => "", :created_at => "2010-11-21")
+#Episode.create!(:podcast => @recordcase, :number => 4, :title => "Spanish Jazz", 				:description => "", :created_at => "2010-12-05")
+#Episode.create!(:podcast => @recordcase, :number => 5, :title => "Survive", 					:description => "", :created_at => "2011-09-24")
+#Episode.create!(:podcast => @recordcase, :number => 6, :title => "John Williams", 				:description => "", :created_at => "2011-11-16")
+#Episode.create!(:podcast => @recordcase, :number => 7, :title => "John Williams die Zweite", 	:description => "", :created_at => "2012-01-01")
+#Episode.create!(:podcast => @recordcase, :number => 8, :title => "Alan Silvestri", 				:description => "", :created_at => "2012-03-12")
+#
+## Episodes Celluloid
+#Episode.create!(:podcast => @celluloid, :number => 1, 	:title => "Pulp Fiction: Tarantino", 																:description => "", :created_at => "2010-09-26")
+#Episode.create!(:podcast => @celluloid, :number => 2, 	:title => "Alfred Hitchcock und Der unsichtbare Dritte", 											:description => "", :created_at => "2010-10-13")
+#Episode.create!(:podcast => @celluloid, :number => 3, 	:title => "David Fincher and The Game.", 															:description => "", :created_at => "2010-11-21")
+#Episode.create!(:podcast => @celluloid, :number => 4, 	:title => "Stanley Kubrick und seine Space Opera!",													:description => "", :created_at => "2010-12-05")
+#Episode.create!(:podcast => @celluloid, :number => 5, 	:title => "Ivan Reitmann und wie man am besten Geister auf der Leinwand faengt", 					:description => "", :created_at => "2011-04-13")
+#Episode.create!(:podcast => @celluloid, :number => 6, 	:title => "Alien und sein Schoepfer Ridley Scott", 													:description => "", :created_at => "2011-06-26")
+#Episode.create!(:podcast => @celluloid, :number => 7, 	:title => "Ian Flemmings- Bond,,,, James Bond", 													:description => "", :created_at => "2011-06-30")
+#Episode.create!(:podcast => @celluloid, :number => 8, 	:title => "Christopher Nolan: Memento und Batman begins", 											:description => "", :created_at => "2011-07-10")
+#Episode.create!(:podcast => @celluloid, :number => 9, 	:title => "Darren Aronofsky Black Swan / The Wrestler",												:description => "", :created_at => "2011-09-21")
+#Episode.create!(:podcast => @celluloid, :number => 10, 	:title => "Steven Spielberg and the green little thing", 											:description => "", :created_at => "2011-11-16")
+#Episode.create!(:podcast => @celluloid, :number => 11, 	:title => "George Lucas Star Wars Saga (oder warum man keine schlechten Prequels drehen sollte)",	:description => "", :created_at => "2011-11-17")
+#Episode.create!(:podcast => @celluloid, :number => 12, 	:title => "Robert Zemeckis und Back to the future", 												:description => "", :created_at => "2012-01-03")
+#Episode.create!(:podcast => @celluloid, :number => 13, 	:title => "Merry Christmas und TOI lieblings Weihnachtsfilme", 										:description => "", :created_at => "2012-01-03")
+#Episode.create!(:podcast => @celluloid, :number => 14, 	:title => "Bud Spencer & Terence Hill",																:description => "", :created_at => "2012-03-12")
+#Episode.create!(:podcast => @celluloid, :number => 15, 	:title => "Peter Jackson", 																			:description => "", :created_at => "2012-06-02")
+#Episode.create!(:podcast => @celluloid, :number => 16,	:title => "Joss Whedon und The Avengers", 															:description => "", :created_at => "2012-06-22")
+#Episode.create!(:podcast => @celluloid, :number => 17,	:title => "Charlie Chaplin", 																		:description => "", :created_at => "2012-07-20")
+#Episode.create!(:podcast => @celluloid, :number => 18,	:title => "Guy Ritchie",																			:description => "", :created_at => "2012-10-19")
+#
+## Pages
+#Page.create!(:titel => "About", :content => "")
+#Page.create!(:titel => "Impressum", :content => "")
+#
+## Blogroll
+#Blogroll.create!(:name => "Code-Chimp", :url => "http://code-chimp.org", :description => "Bastians Tech-Blog")
+#Blogroll.create!(:name => "Movie-Trivia of the Day", :url => "http://movietriviaoftheday.com/", :description => "Bastians Trivia-Seite")
+#Blogroll.create!(:name => "Kinosaal Deluxe", :url => "http://kinosaal-deluxe.de/", :description => "Dimitars Filmrezensionen")
+#Blogroll.create!(:name => "Der Lautsprecher", :url => "http://der-lautsprecher.de/", :description => "Podcast ueber Podcasting von Tim Pritlove")
