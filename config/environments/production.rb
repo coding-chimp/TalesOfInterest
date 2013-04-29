1f76efa8a8b117e63a467ae1066557868fa0fcee
Bastian Bartmann <xarfai27@gmail.com>
2013-04-29 18:17:58 +0200

    cofigured memcached server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ TalesOfInterest::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store
+  config.cache_store = :dalli_store, 'as1.netflower.de'
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
