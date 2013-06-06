abdb24a96ba5b449f08c888116f3a421e4f3311e
Bastian Bartmann <xarfai27@gmail.com>
2013-06-06 19:56:28 +0200

    subscriber chart gets its data now from uri's /readers endpoint

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -13,15 +13,16 @@ module DashboardHelper
   end
 
   def subscribers_chart_data(podcast)
-    uri = URI.parse("http://api.uri.lv/feeds/subscribers.json")
+    uri = URI.parse("http://api.uri.lv/feeds/readers.json")
     params = { :key => Settings.first.uri_key, :token => Settings.first.uri_token, :feed => podcast.downcase }
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
-    if stats = JSON.parse(response.body)['stats']
-      [
-        {label: "Google Reader", value: stats.first['greader']},
-        {label: "Other", value: stats.first['other']}
-      ]
+    if readers = JSON.parse(response.body)['readers']
+      stats = []
+      readers.each do |reader|
+        stats << { label: reader['name'], value: reader['subscribers'] }
+      end
+      stats
     end
   end
 
