aa975516cceb3e3e445bef378ba8fc1ec063676a
Bastian Bartmann <xarfai27@gmail.com>
2013-06-08 13:21:00 +0200

    divided subscribers count in rss and other readers

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -6,7 +6,7 @@ module DashboardHelper
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
     if stats = JSON.parse(response.body)['stats']
-      subscribers = stats.first['greader'] + stats.first['other']
+      "#{stats.first['greader']} / #{stats.first['other']}"
     else
       "ERROR"
     end
