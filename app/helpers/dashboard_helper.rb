88ded351f148d6070711202c10448b2e38077953
Bastian Bartmann <xarfai27@gmail.com>
2013-06-10 20:45:54 +0200

    subscribers count shows only count of rss reader subscribers

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -6,7 +6,7 @@ module DashboardHelper
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
     if stats = JSON.parse(response.body)['stats']
-      "#{stats.first['greader']} / #{stats.first['other']}"
+      stats.first['greader']
     else
       "ERROR"
     end
