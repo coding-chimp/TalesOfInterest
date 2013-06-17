701533019486b828476961037d1250d3e746efda
Bastian Bartmann <xarfai27@gmail.com>
2013-06-17 11:43:31 +0200

    refactored import_helper

--- app/helpers/import_helper.rb
+++ app/helpers/import_helper.rb
@@ -4,25 +4,7 @@ module ImportHelper
   	items = xml.xpath("//channel//item")
   	author = xml.at_xpath("//channel//title").text
   	items.each do |item|
-  	  unless item.at_xpath("title").text.scan(/(.+) \d+:/)[0] == nil
-  	    podcast_name = item.at_xpath("title").text.scan(/(.+) \d+:/)[0][0]
-  	    episode_nr = item.at_xpath("title").text.scan(/(\d+):/)[0][0].to_i
-        content = HTMLPage.new :contents => item.at_xpath("content:encoded").text
-  	    description = content.markdown
-        links = description.scan(/\[([^\]]+)\]\(([^)]+)\)/)
-  	    description = description.gsub("Download #{podcast_name} #{episode_nr.to_s.rjust(3, '0')}",'').gsub("Download Podcast (mp3)",'').gsub(/\[\]\(.*\)/,'')
-  	    if Podcast.find_by_name(podcast_name) == nil
-  	      Podcast.create!(:name => podcast_name, :author => author)
-  	    end
-  	    @ep =  Episode.create!(:podcast => Podcast.find_by_name(podcast_name),
-  	                           :number => episode_nr,
-  	                           :title => item.at_xpath("title").text.scan(/:\D(.+)/)[0][0],
-  	                           :description => description,
-  	                           :file => links.last[1].match(/^[^ ]+/)[0],
-                               :draft => true,
-  	                           :created_at => item.at_xpath("pubDate").text,
-                               :published_at => item.at_xpath("pubDate").text)
-  	  end
+      parse_episode(item, author)
   	end
 	end
 
@@ -30,8 +12,33 @@ module ImportHelper
     items = Nokogiri::XML(params[:import][:file]).xpath("//channel//item")
     items.each do |item|
       text = HTMLPage.new :contents => item.at_xpath("content:encoded").text
-      Page.create!( :title => item.at_xpath("title").text,
-                    :content =>  text.markdown )
+      Page.create!(:title => item.at_xpath("title").text,
+                   :content =>  text.markdown )
+    end
+  end
+
+  private
+
+  def parse_episode(item, author)
+    podcast_name = item.at_xpath("title").text.scan(/(.+) \d+:/)[0][0]
+    unless podcast_name == nil
+      episode_nr = item.at_xpath("title").text.scan(/(\d+):/)[0][0].to_i
+      content = HTMLPage.new :contents => item.at_xpath("content:encoded").text
+      description = content.markdown
+      links = description.scan(/\[([^\]]+)\]\(([^)]+)\)/)
+      description = description.gsub("Download #{podcast_name} #{episode_nr.to_s.rjust(3, '0')}",'').gsub("Download Podcast (mp3)",'').gsub(/\[\]\(.*\)/,'')
+      podcast = Podcast.find_by_name(podcast_name)
+      if  podcast == nil
+        podcast = Podcast.create!(:name => podcast_name, :author => author)
+      end
+      Episode.create!(:podcast => podcast,
+                      :number => episode_nr,
+                      :title => item.at_xpath("title").text.scan(/:\D(.+)/)[0][0],
+                      :description => description,
+                      :file => links.last[1].match(/^[^ ]+/)[0],
+                      :draft => true,
+                      :created_at => item.at_xpath("pubDate").text,
+                      :published_at => item.at_xpath("pubDate").text)
     end
   end
 end
