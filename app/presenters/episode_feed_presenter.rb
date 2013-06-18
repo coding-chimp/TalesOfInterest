908fa57b4702686c0222a3a6e4772afcd5cbdbc3
Bastian Bartmann <xarfai27@gmail.com>
2013-06-18 12:19:31 +0200

    bugfix

--- app/presenters/episode_feed_presenter.rb
+++ app/presenters/episode_feed_presenter.rb
@@ -83,11 +83,5 @@ private
     end
     string
   end
-  
-  def pluralize_without_count(count, noun, text = nil)
-    if count != 0
-      count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
-    end
-  end
 
 end
--- app/presenters/episode_presenter.rb
+++ app/presenters/episode_presenter.rb
@@ -95,4 +95,10 @@ private
     end
     faulty_files
   end
+
+  def pluralize_without_count(count, noun, text = nil)
+    if count != 0
+      count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
+    end
+  end
 end
