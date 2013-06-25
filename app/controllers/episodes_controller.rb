3a42148523ff4f91888ec6903862ff5e63185b0e
Bastian Bartmann <xarfai27@gmail.com>
2013-06-25 16:19:44 +0200

    refactored episodes controller a bit

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -22,45 +22,42 @@ class EpisodesController < ApplicationController
 	end
 
 	def show
-		@podcast = Podcast.find(params[:podcast])
-		@episode = @podcast.latest_episode
+		podcast = Podcast.find(params[:podcast])
+		@episode = podcast.latest_episode
 		if params[:id].to_i > @episode.number
-			redirect_to episode_path(@podcast, @episode), alert: "An episode with number #{params[:id]} is not available. This is the most recent episode."
+			redirect_to episode_path(podcast, @episode), alert: "An episode with number #{params[:id]} is not available. This is the most recent episode."
 		else
-			@episode = @podcast.episodes.find(params[:id])
+			@episode = podcast.episodes.find(params[:id])
 		end
 	end
 
 	def new
-		@podcast = Podcast.find(params[:podcast])
+		podcast = Podcast.find(params[:podcast])
 		@episode = Episode.new
-		@episode.podcast = @podcast
+		@episode.podcast = podcast
 		url = @episode.set_file_url
 		audio_file = AudioFile.new(url: url, media_type: "mp4", episode_id: @episode.id)
 		@episode.audio_files << audio_file
-		@show_notes = @episode.show_notes
 	end
 
 	def create
-		@podcast = Podcast.find(params[:podcast])
+		podcast = Podcast.find(params[:podcast])
 		@episode = Episode.includes(:podcast, :show_notes).new(params[:episode])
-		@show_notes = @episode.show_notes
 
 		if @episode.save
 			if params[:publish]
 				@episode.draft = false
 				if @episode.valid?
 					@episode.save
-					redirect_to(podcast_episodes_path(@episode.podcast), notice: 'Episode was successfully published.')
+					redirect_to(podcast_episodes_path(podcast), notice: 'Episode was successfully published.')
 				else
-					@episode.draft = true
-					@episode.published_at = nil
+					@episode.update_attributes(draft: true, published_at: nil)
 					render action: "new"
 				end
 			elsif params[:save_close]
-				redirect_to(podcast_episodes_path(@podcast), notice: 'Episode was successfully saved.')
+				redirect_to(podcast_episodes_path(podcast), notice: 'Episode was successfully saved.')
 			else
-				redirect_to(edit_episode_path(@podcast, @episode), notice: 'Episode was successfully saved.')
+				redirect_to(edit_episode_path(podcast, @episode), notice: 'Episode was successfully saved.')
 			end
 		else
 			render action: "new"
@@ -68,31 +65,28 @@ class EpisodesController < ApplicationController
 	end
 
 	def edit
-		@podcast = Podcast.find(params[:podcast])
-		@episode = @podcast.episodes.find(params[:id])
-		@show_notes = @episode.show_notes
+		podcast = Podcast.find(params[:podcast])
+		@episode = podcast.episodes.find(params[:id])
 	end
 
 	def update
-		@podcast = Podcast.find(params[:podcast])
-		@episode = @podcast.episodes.find(params[:id])
-		@show_notes = @episode.show_notes
+		podcast = Podcast.find(params[:podcast])
+		@episode = podcast.episodes.find(params[:id])
 
 		if @episode.update_attributes(params[:episode])
 			if params[:publish]
 				@episode.draft = false
 				if @episode.valid?
 					@episode.save
-					redirect_to(podcast_episodes_path(@podcast), notice: 'Episode was successfully published.')
+					redirect_to(podcast_episodes_path(podcast), notice: 'Episode was successfully published.')
 				else
-					@episode.draft = true
-					@episode.published_at = nil
+					@episode.update_attributes(draft: true, published_at: nil)
 					render action: "edit"
 				end
 			elsif params[:save_close]
-				redirect_to(podcast_episodes_path(@podcast), notice: 'Episode was successfully saved.')
+				redirect_to(podcast_episodes_path(podcast), notice: 'Episode was successfully saved.')
 			else
-				redirect_to(edit_episode_path(@podcast, @episode), notice: 'Episode was successfully saved.')
+				redirect_to(edit_episode_path(podcast, @episode), notice: 'Episode was successfully saved.')
 			end
 		else
 			render action: "edit"
@@ -100,15 +94,15 @@ class EpisodesController < ApplicationController
 	end
 
 	def destroy
-		@podcast = Podcast.find(params[:podcast])
-		@episode = @podcast.episodes.find(params[:id])
+		podcast = Podcast.find(params[:podcast])
+		@episode = podcast.episodes.find(params[:id])
 		@episode.destroy
 		redirect_to podcast_episodes_path
 	end
 
 	def latest
-		@podcast = Podcast.find(params[:podcast])
-		@episode = @podcast.episodes.published.recent.first
-		redirect_to episode_url(@podcast, @episode)
+		podcast = Podcast.find(params[:podcast])
+		@episode = podcast.episodes.published.recent.first
+		redirect_to episode_url(podcast, @episode)
 	end
 end
